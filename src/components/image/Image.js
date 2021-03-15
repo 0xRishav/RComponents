dcfb8b55f2cc5bfe3b49050762312b65f480005e
Rishav Bharti <rshvbharti@gmail.com>
2021-03-15 23:22:29 +0530

    style: changed the font size of image text

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -9,7 +9,7 @@ function Image({ type }) {
         src="https://images.unsplash.com/photo-1506499984411-860f50ae4acb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
         alt="image"
       />
-      <h4>{type} Image</h4>
+      <h3>{type} Image</h3>
     </div>
   );
 }
